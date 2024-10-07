7e5253c34d77ef63319c2354edb8d8252aca3ba8
annasashkova <avlaada99@gmail.com>
2024-10-07 20:07:06 +0300

    add changes

--- src/main/java/ru/annasashkova/calculator/service/ResultWriterService.java
+++ src/main/java/ru/annasashkova/calculator/service/ResultWriterService.java
@@ -1,7 +1,7 @@
 package ru.annasashkova.calculator.service;
 
 public class ResultWriterService {
-    public static void printResult(String operation, int num1, int num2, int res){
-        System.out.println(num1 + operation + num2 + "=" + res);
+    public static void printResult(String operation, int res){
+        System.out.println("Результ " + operation + " равен " + res);
     }
 }
--- src/main/java/ru/annasashkova/calculator/util/Calculator.java
+++ src/main/java/ru/annasashkova/calculator/util/Calculator.java
@@ -3,18 +3,26 @@ package ru.annasashkova.calculator.util;
 import java.util.Scanner;
 
 public class Calculator {
-    public static int sum(int x, int y) {
-        int res = x + y;
-        return res;
+    public static int sum() {
+        int x = scanner();
+        int y = scanner();
+        return x + y;
     }
 
-    public static int multiply(int x, int y) {
-        int res = x * y;
-        return res;
+    public static int multiply() {
+        int x = scanner();
+        int y= scanner();
+        return x * y;
     }
 
-    public static int minus(int x, int y) {
-        int res = x - y;
-        return res;
+    public static int minus() {
+        int x = scanner();
+        int y= scanner();
+        return x - y;
+    }
+
+    public static int scanner() {
+        Scanner sc = new Scanner(System.in);
+        return sc.nextInt();
     }
 }
